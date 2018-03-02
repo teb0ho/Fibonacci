ab75c66463a29106b1a026a5a25bd1391c90579f
Teboho Mphure <tebohomphure@gmail.com>
2018-03-02 12:50:38 +0200

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,25 @@
+def fib(n):
+    
+    a, b = 0, 1
+    final_output = str(a) + ' ' + str(b)
+
+    if n == 2:
+        return final_output
+    elif n == 1:
+        return '0'
+    else:
+        count = 2
+        while (count < n):
+            if count < n:
+                a = a + b
+                final_output += ' ' + str(a)
+                count += 1
+            if count < n:
+                b = a + b
+                final_output += ' ' + str(b)
+                count += 1
+
+        return final_output
+
+fib_length = int(raw_input('Enter the length you want for your fibonacci sequence? '))
+print fib(fib_length) 
